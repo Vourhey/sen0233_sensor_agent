86bc9593456b5a1fcf911d2eb0bae1a53ad2259f
Vadim Manaenko <vadim.razorq@gmail.com>
2019-12-12 12:42:05 +0300

    Add concurent thread to read data

--- src/sensors_pack_agent/sensors.py
+++ src/sensors_pack_agent/sensors.py
@@ -1,4 +1,5 @@
 import serial
+from threading import Timer
 import rospy
 
 class SEN0233:
@@ -7,42 +8,48 @@ class SEN0233:
 
         self.serialPort = serial.Serial(physicalPort)
 
+        def read_most_recent_data():
+            if self.serialPort.in_waiting >= 40:
+                self.data_from_sensor = self.serialPort.read(40)
+
+            Timer(0.7, read_most_recent_data).start() # maximum recommended delay
+        read_most_recent_data()
+
     def data(self) -> dict:
         rospy.loginfo("In waiting data: {}".format(self.serialPort.in_waiting))
-        if self.serialPort.in_waiting >= 40:
-            # Check that we are reading the payload from the correct place (i.e. the start bits)
-            data = self.serialPort.read(40)
 
-            CR1 =(data[38]<<8) + data[39];
-            CR2 = 0;
-            for i in range(38):
-                CR2 += data[i]
+        d = self.data_from_sensor
 
-            if CR1 == CR2:
-                PMSa=data[12]         #Read PM2.5 High 8-bit
-                PMSb=data[13]         #Read PM2.5 Low 8-bit
-                PMS=(PMSa<<8)+PMSb          #PM2.5 value
-                FMHDSa=data[28]         #Read Formaldehyde High 8-bit
-                FMHDSb=data[29]         #Read Formaldehyde Low 8-bit
-                FMHDS=(FMHDSa<<8)+FMHDSb     #Formaldehyde value
-                TPSa=data[30]          #Read Temperature High 8-bit
-                TPSb=data[31]          #Read Temperature Low 8-bit
-                TPS=(TPSa<<8)+TPSb        #Temperature value
-                HDSa=data[32]          #Read Humidity High 8-bit
-                HDSb=data[33]          #Read Humidity Low 8-bit
-                HDS=(HDSa<<8)+HDSb      #Humidity value
-            else:
-                PMS = 0
-                FMHDS = 0
-                TPS = 0
-                HDS = 0
+        CR1 =(d[38]<<8) + d[39]
+        CR2 = 0
+        for i in range(38):
+            CR2 += d[i]
 
-            res = {
-                    "temp_c": round(TPS/10, 2),
-                    "humidity": round(HDS/10, 2),
-                    "formaldehyde": FMHDS,
-                    "pm2.5": PMS
-                    }
-            rospy.loginfo(res)
-            return res
+        if CR1 == CR2:
+            PMSa = d[12]             # Read PM2.5 High 8-bit
+            PMSb = d[13]             # Read PM2.5 Low 8-bit
+            PMS = (PMSa<<8)+PMSb        # PM2.5 value
+            FMHDSa = d[28]           # Read Formaldehyde High 8-bit
+            FMHDSb = d[29]           # Read Formaldehyde Low 8-bit
+            FMHDS = (FMHDSa<<8)+FMHDSb  # Formaldehyde value
+            TPSa = d[30]             # Read Temperature High 8-bit
+            TPSb = d[31]             # Read Temperature Low 8-bit
+            TPS = (TPSa<<8)+TPSb        # Temperature value
+            HDSa = d[32]             # Read Humidity High 8-bit
+            HDSb = d[33]             # Read Humidity Low 8-bit
+            HDS = (HDSa<<8)+HDSb        # Humidity value
+        else:
+            PMS = 0
+            FMHDS = 0
+            TPS = 0
+            HDS = 0
+
+        res = {
+                "temp_c": round(TPS/10, 2),
+                "humidity": round(HDS/10, 2),
+                "formaldehyde": FMHDS,
+                "pm2.5": PMS
+                }
+        rospy.loginfo(res)
+        return res
 
